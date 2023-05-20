f6facf5a14353d6ba9ad81a18dc6c816d4f0a756
Tina <leskovskaya.ti@gmail.com>
2023-05-20 23:40:57 +0300

    убирает evt.preventDefault

--- src/view/list-sort-view.js
+++ src/view/list-sort-view.js
@@ -7,7 +7,7 @@ function createListSortTemplate(currentSortType) {
 
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   <div class="trip-sort__item  trip-sort__item--day">
-    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SortType.DAY}" ${isChecked()}>
+    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SortType.DAY}" ${isChecked(SortType.DAY)}>
     <label class="trip-sort__btn" for="sort-day">Day</label>
   </div>
 
@@ -17,12 +17,12 @@ function createListSortTemplate(currentSortType) {
   </div>
 
   <div class="trip-sort__item  trip-sort__item--time">
-    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SortType.TIME}" ${isChecked()}>
+    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SortType.TIME}" ${isChecked(SortType.TIME)}>
     <label class="trip-sort__btn" for="sort-time">Time</label>
   </div>
 
   <div class="trip-sort__item  trip-sort__item--price">
-    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SortType.PRICE}" ${isChecked()}>
+    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SortType.PRICE}" ${isChecked(SortType.PRICE)}>
     <label class="trip-sort__btn" for="sort-price">Price</label>
   </div>
 
@@ -54,7 +54,7 @@ export default class ListSortView extends AbstractView {
       return;
     }
 
-    evt.preventDefault();
+    //evt.preventDefault();
     this.#handleSortTypeChange(evt.target.dataset.sortType);
   };
 }
